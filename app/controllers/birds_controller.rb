81bd7545496de5f8d00b403f72429b8a87e61e8a
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-26 09:17:51 +0300

    commit tree faf6c874375b0ad919071de292f96f53b591c2ad

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -8,7 +8,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    #bird = Bird.create(params.permit(:name, :species))
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -21,5 +22,10 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  private
+  # all methods below here are private
 
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
